c08cf866b70b146f278b91069ece485cb77032ab
Tom <tomaszsobkowicz21@gmail.com>
2020-09-26 20:30:42 +0200

    Express and mongoDB setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// cConnect Database
+connectDB();
+
+// test route
+app.get('/', (req, res) => res.send('API running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;
+// eslint-disable-next-line no-console
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
