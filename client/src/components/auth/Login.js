a3f3464aa0a63210fe458a21fb5251eb87d330ea
Tom <tomaszsobkowicz21@gmail.com>
2020-09-30 17:51:39 +0200

    Set up react client app and concurrently yarn script, add proxy to client app, create navbar, landing, login and register components

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-console */
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('success');
+  };
+
+  return (
+    <>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            minLength="6"
+          />
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign In</Link>
+      </p>
+    </>
+  );
+};
+
+export default Login;
